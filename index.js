66350b1bd8bbe7e7806c33244f33c30a8c66b57d
picsacoder <66335345+picsacoder@users.noreply.github.com>
2021-05-21 08:11:47 -0500

    Primeros pasos :sunflower:

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
+function ChangeImage() {
+  const url = "https://dog.ceo/api/breeds/image/random"
 
-function a(){ 
-    
-    var img_src = new XMLHttpRequest()
-    img_src.addEventListener("load", reqListener);
-    img_src.open("GET", "https://dog.ceo/api/breeds/image/random")
-    
-    var x = document.getElementById("perro").src = x
+  fetch(url)
+    .then(response => response.json())
+    .then(data => { 
+      
+      var id_data = document.getElementById("imagen_perro").src = data.message
+      
+    })
+  
+  
+    .catch(err => console.log(err))
+
 }
 
-function reqListener () {
-    console.log(this.responseText);
-  }
+
+ChangeImage()
+
+
+
+function elegirRaza() { 
+
+
+
+  let tipo_raza = "Belga-malinois" //variable que cambie cuando se escoge una raza
+
+  const url_razas = {
+    "Belga-malinois": "https://dog.ceo/api/breed/malinois/images/random",
+    "Boxer": "https://dog.ceo/api/breed/boxer/images/random",
+    "Shiba": "https://dog.ceo/api/breed/shiba/images/random",  
+    "Chihuahua": "https://dog.ceo/api/breed/chihuahua/images/random",
+    "Akita": "https://dog.ceo/api/breed/akita/images/random",
+    "Husky": "https://dog.ceo/api/breed/Husky/images/random",
   
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://dog.ceo/api/breeds/image/random");
-oReq.send();
+
+  }
+
+  var url_raza = url_razas[tipo_raza]; //Url de la imagen a fetch
+
+  console.log(url_raza)
+
+
+  fetch(url_raza)
+    .then(response => response.json())
+    .then(data => { 
+      var id_data = document.getElementById("perro_esp").src = data.message
+    })
+
+
+    .catch(err => console.log(err))
+}
+
+elegirRaza()
